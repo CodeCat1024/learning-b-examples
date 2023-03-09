5272cf679873769ff426b9630dc27f5243c7cb53
CodeCat <2720883600@qq.com>
2023-03-09 22:56:17 +0800

    added a spring boot logger example

---
+++ Examples/SpringExamples/HelloSpringBoot/src/main/java/HelloSpringBoot/main/logger.java
@@ -0,0 +1,25 @@
+package HelloSpringBoot.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@ResponseBody
+public class logger {
+    //获取日志对象（来自slf4j）
+    private final static Logger log =
+            LoggerFactory.getLogger(logger.class);
+
+    @RequestMapping("/logger")
+    public void sayHi() {
+        log.trace("我是 trace");
+        log.debug("我是 debug");
+        log.info("我是 info");
+        log.warn("我是 warn");
+        log.error("我是 error");
+    }
+}
