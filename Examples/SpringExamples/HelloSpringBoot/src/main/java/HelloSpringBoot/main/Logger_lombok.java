36f8ea2430673abf1970f861f3f6c12a56c650bc
CodeCat <2720883600@qq.com>
2023-03-12 12:40:44 +0800

    added logger_lombok and so on

--- Examples/SpringExamples/HelloSpringBoot/src/main/java/HelloSpringBoot/main/Logger_lombok.java
+++ Examples/SpringExamples/HelloSpringBoot/src/main/java/HelloSpringBoot/main/Logger_lombok.java
@@ -1,4 +1,10 @@
 package HelloSpringBoot.main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class Logger_lombok {
+
 }
