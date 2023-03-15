78fdda2c53a5a962a0bd54e10f084d397a899365
CodeCat <2720883600@qq.com>
2023-03-15 10:51:17 +0800

    update

--- Examples/SpringExamples/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Examples/SpringExamples/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println();
+
+		logger.trace("我是 trace");
+		logger.debug("我是 debug");
+		logger.info("我是 info");
+		logger.warn("我是 warn");
+		logger.error("我是 error");
 	}
 
 }
