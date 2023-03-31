6c4884058ea450252ca151aeb6c561c7b1c99eb7
CodeCat <2720883600@qq.com>
2023-03-31 10:04:32 +0800

    added

--- Projects/SmallBlog/src/main/java/com/lrm/dao/TypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lrm.dao;
-
-import com.lrm.po.Type;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeRepository extends JpaRepository<Type, Long> {
-    Type findByName(String name);
-}
