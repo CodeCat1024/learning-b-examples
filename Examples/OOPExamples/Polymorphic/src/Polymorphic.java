dc70af8e2e55491566214b978b7e395435f745f7
CodeCat <2720883600@qq.com>
2023-04-15 13:38:39 +0800

    增加了继承实例

---
+++ Examples/OOPExamples/Polymorphic/src/Polymorphic.java
@@ -0,0 +1,58 @@
+abstract class Shape {
+    public abstract void draw();
+}
+
+class Rect extends Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("画矩形！");
+    }
+}
+
+class Cycle extends Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("画圆圈");
+    }
+}
+
+class Flower extends Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("画花");
+    }
+}
+
+public class Polymorphic {
+    public static void drawMap(Shape shape) {
+        shape.draw();
+    }
+
+    public static void main(String[] args) {
+        Rect rect = new Rect();
+        Cycle cycle = new Cycle();
+        Flower flower = new Flower();
+
+        Shape[] shapes = {rect, cycle, flower};
+        for (Shape shape : shapes) {
+            shape.draw();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
