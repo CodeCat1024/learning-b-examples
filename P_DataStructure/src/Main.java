6669b3a9af1034257cb1248b817538190621e02e
zjt <2720883600@qq.com>
2023-07-28 18:05:35 +0800

    commit

--- P_DataStructure/src/Main.java
+++ P_DataStructure/src/Main.java
@@ -1,24 +1,5 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list1 = new LinkedList<>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        list1.add(4);
-        list1.add(5);
-        list1.add(6);
 
-        List<Integer> list2 = list1.subList(0, 3);
-        System.out.println(list2);
-
-        list2.set(1, 1000);
-
-        System.out.println(list1);
-        System.out.println(list2);
     }
 }
